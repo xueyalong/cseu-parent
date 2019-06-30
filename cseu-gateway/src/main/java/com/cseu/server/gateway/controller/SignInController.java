f094d5267cb8173b55d3977b52fbb8509be147ef
xueyalong <xueyl101@163.com>
2019-06-30 19:38:32 +0800

    项目结构进行了调整，使用maven模块化管理进行代码管理

---
+++ cseu-gateway/src/main/java/com/cseu/server/gateway/controller/SignInController.java
@@ -0,0 +1,54 @@
+package com.cseu.server.gateway.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cseu.server.gateway.auth.Jwt;
+import com.cseu.server.gateway.base.exception.CesuException;
+import com.cseu.server.gateway.service.CseuGuestService;
+import com.cseu.server.user.api.CseuGuestRpcService;
+import com.cseu.server.user.bean.CseuGuest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+/**
+ * @title: SignInController
+ * @projectName cseu-parent
+ * @description: TODO
+ * @author: yalong.xue
+ * @date 2019-06-3004:06
+ */
+
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+public class SignInController {
+
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/refesh")
+    @ResponseBody
+    public Object login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        CseuGuest cseuGuest = CseuGuest.builder().build().selectOne(new QueryWrapper<CseuGuest>().eq("user_count",username));
+        if (Objects.nonNull(cseuGuest)) {
+            return Jwt.saveToken(redisTemplate,username);
+        }
+        return CesuException.ErrorEnum.USER_NOT_EXISTS;
+    }
+
+    @GetMapping("/token")
+    @ResponseBody
+    public Object token(@RequestParam("username") String username, @RequestParam("password") String password) {
+        CseuGuest cseuGuest =CseuGuest.builder().build().selectOne(new QueryWrapper<CseuGuest>().eq("user_count",username));
+        if (Objects.nonNull(cseuGuest)) {
+            return Jwt.createJwt(cseuGuest.getUserCount());
+        }
+        return CesuException.ErrorEnum.USER_NOT_EXISTS;
+    }
+}
