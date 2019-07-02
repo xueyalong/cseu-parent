fe10e6d68e90c527e440460ed97154e349241901
xueyalong <xueyl101@163.com>
2019-07-02 10:06:08 +0800

    添加配置中心

--- cseu-user/src/main/java/com/cseu/server/user/UserApplication.java
+++ cseu-user/src/main/java/com/cseu/server/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.cseu.server.user;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
